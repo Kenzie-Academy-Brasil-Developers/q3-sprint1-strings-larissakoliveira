082182424cc037dac86f0a85ead3bef69acec1d2
larissakoliveira <oliveir5uwm@gmail.com>
2021-12-07 13:05:20 -0300

    strings done

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Código do dev aqui
+def standardize_names(name):
+    remove_black_spaces = name.strip()
+    split_words =  remove_black_spaces.split(' ')
+    words_len = len(split_words)
+    if words_len == 1:
+        return remove_black_spaces
+    else:
+        return str('-'.join(split_words))
+
+hero_standardized = standardize_names(" Batman     ")
+print(hero_standardized)
+
+hero_standardized = standardize_names("      Super Man")
+print(hero_standardized)
